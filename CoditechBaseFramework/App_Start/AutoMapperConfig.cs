4155fe224ccfaafadbf2efba3e4bc5339799fabe
anirudha kanade <anirudha.kanade@amla.io>
2024-04-28 23:36:56 +0530

    code correction

---
+++ CoditechBaseFramework/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+using Coditech.Utilities.Filters;
+
+namespace Coditech
+{
+    public static class AutoMapperConfig
+    {
+        public static void Execute()
+        {
+            Mapper.CreateMap<FilterTuple, FilterDataTuple>();
+        }
+    }
+}
+
