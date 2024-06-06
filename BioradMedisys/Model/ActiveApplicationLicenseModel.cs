911c21ad36d51ebecef78b0a8b8ec43f47699fa2
anirudha kanade <anirudha.kanade@amla.io>
2024-06-07 00:23:57 +0530

    code correction

--- BioradMedisys/Model/ActiveApplicationLicenseModel.cs
+++ BioradMedisys/Model/ActiveApplicationLicenseModel.cs
@@ -1,7 +1,16 @@
-﻿namespace Coditech.Model
+﻿using System;
+
+namespace Coditech.Model
 {
-    public class ActiveApplicationLicenseModel : BaseModel
+    public class ActiveApplicationLicenseModel 
     {
         public bool IsActive { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public bool HasError { get; set; } = false;
+        public string ErrorMessage { get; set; }
+        public int ErrorCode { get; set; }
     }
 }
