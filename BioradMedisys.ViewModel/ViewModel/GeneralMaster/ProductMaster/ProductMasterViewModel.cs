61668cdf1bd3c4577c0429aba4f35a9bd7370d64
Anirudha Kanade <anirudhakanade1007@gmail.com>
2024-10-23 07:41:21 +0530

    code correction

--- BioradMedisys.ViewModel/ViewModel/GeneralMaster/ProductMaster/ProductMasterViewModel.cs
+++ BioradMedisys.ViewModel/ViewModel/GeneralMaster/ProductMaster/ProductMasterViewModel.cs
@@ -8,7 +8,7 @@ namespace Coditech.ViewModel
     {
         public int ProductMasterId { get; set; }
         [Required]
-        [Display(Name = "Department Name")]
+        [Display(Name = "Product Name")]
         public string ProductName { get; set; }
         public string ProductUniqueCode { get; set; }
         public string FileName { get; set; }
