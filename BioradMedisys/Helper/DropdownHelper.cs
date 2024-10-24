7208cdf92c5b137d7239f6e8c5fc04c2a02a21c4
ayu-gumgaonkar <ayushigumgaonkar8@gmail.com>
2024-10-24 09:05:11 +0530

    Added the Dropdown of Role Name in the Edit page.

--- BioradMedisys/Helper/DropdownHelper.cs
+++ BioradMedisys/Helper/DropdownHelper.cs
@@ -1,7 +1,40 @@
-﻿namespace Coditech.DropdownHelper
+﻿using Coditech.DataAccessLayer;
+using Coditech.Model;
+using Coditech.ViewModel;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Coditech.DropdownHelper
 {
     public static class DropdownHelper
     {
-      
+        private static readonly AdminRoleMasterDAL _adminRoleMasterDAL = new AdminRoleMasterDAL();
+
+        public static DropdownViewModel GeneralDropdownList(DropdownViewModel dropdownViewModel)
+        {
+            List<SelectListItem> dropdownList = new List<SelectListItem>();
+            if (Equals(dropdownViewModel.DropdownType, "User"))
+            {
+                GetUserRoleDropDown(dropdownViewModel, dropdownList);
+            }
+            dropdownViewModel.DropdownList = dropdownList;
+            return dropdownViewModel;
+        }
+
+        private static void GetUserRoleDropDown(DropdownViewModel dropdownViewModel, List<SelectListItem> dropdownList)
+        {
+            AdminRoleMasterListModel AdminRoleMasterList = _adminRoleMasterDAL.GetAdminRoleList();
+            dropdownList.Add(new SelectListItem() { Text = "-------Select Role Name-------" });
+            foreach (var item in AdminRoleMasterList.AdminRoleMasterList)
+            {
+                dropdownList.Add(new SelectListItem()
+                {
+                    Text = item.RoleName,
+                    Value = item.AdminRoleMasterId.ToString()
+                    //Selected = dropdownViewModel.DropdownSelectedValue == Convert.ToString(item.IsActive)
+                });
+            }
+
+        }
     }
 }
